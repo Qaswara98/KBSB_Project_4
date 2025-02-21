613565361b013e0d3b73fccf9db39fc168879afe
ChristosTricopoulos <143614349+ChristosTricopoulos@users.noreply.github.com>
2025-02-21 08:38:53 +0100

    Add files via upload

---
+++ data analysis.R
@@ -0,0 +1,7 @@
+data <- read.table("Project4.csv", header = TRUE)
+
+data <- subset(data, select = - c(age, height.cm., weight.kg., bmi, systolic, diastolic, sex))  # Remove a single column
+
+library(tidyverse)
+data <- data %>% column_to_rownames(.,'object_id')
+               
