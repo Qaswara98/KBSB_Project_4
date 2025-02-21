9e2be29e5e421d2aa4543b47b6678fdb53c8a648
ChristosTricopoulos <143614349+ChristosTricopoulos@users.noreply.github.com>
2025-02-21 08:39:49 +0100

    Update data analysis.R
    removed an unnecessary comment

--- data analysis.R
+++ data analysis.R
@@ -1,6 +1,6 @@
 data <- read.table("Project4.csv", header = TRUE)
 
-data <- subset(data, select = - c(age, height.cm., weight.kg., bmi, systolic, diastolic, sex))  # Remove a single column
+data <- subset(data, select = - c(age, height.cm., weight.kg., bmi, systolic, diastolic, sex)) 
 
 library(tidyverse)
 data <- data %>% column_to_rownames(.,'object_id')
